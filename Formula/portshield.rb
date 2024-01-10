01e48f22d7ff4193f789989fcb278ba10f4523bc
ikotun-dev <danlogan2003@gmail.com>
2024-01-10 23:06:42 +0100

    changes shasum

--- Formula/portshield.rb
+++ Formula/portshield.rb
@@ -5,21 +5,15 @@ class Portshield < Formula
   sha256 "46926ef81039c26f671a1d98878f8582588b63dcb5e814d96511bdcbe7fcb0cf"
   license "MIT"
 
-  depends_on "python@3.11"
+  depends_on "python"
 
   def install
+    virtualenv_install_with_resources
     bin.install "main.py" => "portshield"
+  
   end
 
   test do
-    output = IO.popen("#{bin}/portshield", "r+") do |pipe|
-      pipe.puts "k"
-      pipe.puts "8080"
-      pipe.puts "y"
-      pipe.close_write
-
-      pipe.read
-    end
-    assert_match "Process with PID", output
+    assert_match "Usage:", shell_output("#{bin}/portshield --help")
   end
 end

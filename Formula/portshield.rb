a6e9bc19d6a50b8874d7082b59eb1a68b4737bac
ikotun-dev <danlogan2003@gmail.com>
2024-01-10 22:34:29 +0100

    changes shasum

--- Formula/portshield.rb
+++ Formula/portshield.rb
@@ -1,8 +1,8 @@
 class Portshield < Formula
   desc "Close opened ports (running processes) easily from CLI"
   homepage "https://github.com/ikotun-dev/port.shield"
-  url "https://github.com/ikotun-dev/port.shield/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "b48f680902f9404da59f1b5598d2879cd2e637627fadd9c0a4b4a5dfb4bb100a"
+  url "https://github.com/ikotun-dev/homebrew-portshield/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "46926ef81039c26f671a1d98878f8582588b63dcb5e814d96511bdcbe7fcb0cf"
   license "MIT"
 
   depends_on "python@3.11"
